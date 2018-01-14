b91e62179d9adde680b01b277ca9ef6c66f32de6
Junki Ishikawa <ishikawa.j@cvlab.cs.tsukuba.ac.jp>
2018-01-15 01:52:06 +0900

    implement basic utilities in cvt/utils/base.py

---
+++ cvt/utils/base.py
@@ -0,0 +1,76 @@
+"""
+Mathematical utilities
+"""
+
+# Authors: Junki Ishikawa
+#          Takahiro Inagaki
+
+import numpy as np
+
+
+def canonical_angle(X, Y):
+    """
+    Calculate cannonical angles beween subspaces
+
+    Parameters
+    ----------
+    X: basis matrix, array-like, shape: (n_subdim_X, n_dim)
+    Y: basis matrix, array-like, shape: (n_subdim_Y, n_dim)
+
+    Returns
+    -------
+    c: float, similarity of X, Y
+
+    """
+
+    _, s, _ = np.linalg.svd(Y @ X.T)
+    c = (s ** 2).mean()
+    return c
+
+
+def canonical_angle_matrix(X, Y):
+    """Calculate canonical angles between subspaces
+    example     similarity = MathUtils.calc_basis_vector(X, Y)
+
+    Parameters
+    ----------
+    X: set of basis matrix, array-like, shape: (n_set_X, n_subdim, n_dim)
+        n_subdim can be variable on each subspaces
+    Y: set of basis matrix, array-like, shape: (n_set_Y, n_subdim, n_dim)
+        n_set can be variable from n_set of X
+        n_subdim can be variable on each subspaces
+
+    Returns:
+        C: similarity matrix, array-like, shape: (n_set_X, n_set_Y)
+
+    """
+
+    n_set_X, n_set_Y = len(X), len(Y)
+    C = np.zeros((n_set_X, n_set_Y))
+    for x in range(n_set_X):
+        for y in range(n_set_Y):
+            C[x, y] = canonical_angle(X[x], Y[y])
+
+    return C
+
+
+def subspace_bases(X, n_subdims):
+    """
+    Simple PCA and
+
+    Parameters
+    ----------
+    X: array-like, shape (n_vectors, n_features)
+        data matrix
+    n_subdims: integer
+        number of subspace dimension
+
+    Returns
+    -------
+    V: array-like, shape (n_subdims, n_features)
+        bases matrix
+    """
+    _, v = np.linalg.eigh(X.T @ X)
+    v = v.T[::-1]
+    V = v[:n_subdims]
+    return V
